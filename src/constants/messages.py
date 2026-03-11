4e213814cef7cb6fa68f318037ac8b6a35689d31
Die <dlibonati@leafnoise.io>
2026-03-10 22:47:50 -0300

    fix: order messages.py

--- src/constants/messages.py
+++ src/constants/messages.py
@@ -2,7 +2,9 @@ MESSAGE_SUCCESS_LOGIN = "You have successfully logged in."
 MESSAGE_SUCCESS_REGISTER = "The user was successfully created."
 
 MESSAGE_ERROR_USER_NOT_EXISTS = "The entered username does not exist in our database."
+
 MESSAGE_ERROR_USERNAME_ALREADY_EXISTS = "The username already exists."
+
 MESSAGE_ERROR_NOT_VALID_PASSWORD = "The password does not match the user entered."
 MESSAGE_ERROR_NOT_VALID_MATCH_PASSWORD = "Passwords are not the same."
 MESSAGE_ERROR_NOT_VALID_FIELDS = "The fields entered are invalid."
